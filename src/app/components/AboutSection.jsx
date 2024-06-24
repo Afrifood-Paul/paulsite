094a3ffced551e9e31627310af9f2bd836305304
Afrifood-Paul <docpaul2016@gmail.com>
2024-06-24 08:38:06 -0700

    first commit

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useTransition } from "react";
-import TabButton from "./tabbutton";
+import TabButton from "./TabButton";
 import Image from "next/image";
 
 const TAB_DATA = [
